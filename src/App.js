9a0a514aa1261e56ffe73096de98b599a4a5098c
navid.asadipur <navid.asadipur@gmail.com>
2022-12-16 13:51:20 +0330

    add backdrop css and row and cols from bootstrap

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
+      <div className="row">
       <Todo title="Learn React"/>
       <Todo title="Master in React"/>
       <Todo title="Explore the full react course"/>
+      </div>
       <Modal />
       <Backdrop />
     </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,17 @@
 function Todo(props) {
-
-    function deleteHandler(){
-        console.log("delete clicked");
-    }
+  function deleteHandler() {
+    console.log("delete clicked");
+  }
 
   return (
-    <div className="card m-3">
-      <h2 className="card-title">{props.title}</h2>
-      <div className="card-footer">
-        <button className="btn btn-danger" onClick={deleteHandler}>Delete</button>
+    <div className="col-4">
+      <div className="card m-3">
+        <h2 className="card-title">{props.title}</h2>
+        <div className="card-footer">
+          <button className="btn btn-danger" onClick={deleteHandler}>
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
