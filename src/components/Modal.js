139f907c520c542ac685462018a21904d5b747c8
navid.asadipur <navid.asadipur@gmail.com>
2022-12-16 13:52:47 +0330

    modify modal class

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,13 @@
 function Modal() {
   return (
-    <div className="fixed-top top-50 d-flex justify-content-center align-items-center">
-    <div className="card p-3 ">
-      <p>Are you sure?</p>
-      <div className="d-flex justify-content-around">
-      <button className="btn btn-success">Cancle</button>
-      <button className="btn btn-danger">Confirm</button>
+    <div className="fixed-top  top-50 d-flex justify-content-center align-items-center">
+      <div className="card p-3 w-25">
+        <p>Are you sure?</p>
+        <div className="d-flex justify-content-around">
+          <button className="btn btn-success">Cancle</button>
+          <button className="btn btn-danger">Confirm</button>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
